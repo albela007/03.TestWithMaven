59ed3150df3171a75227e2194c1b478154ed44b6
LP <reachfast@yahoo.com>
2017-07-14 15:18:37 -0400

    Fix scenario Git-Jenkins failed scenario

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -12,7 +12,7 @@ public class CarTest {
 		CarRepository carRep = new CarRepository();
 		int size = carRep.findMaps().size();
 
-		assertTrue("size is : " + size, size == 20); // this will fail becaue we
+		assertTrue("size is : " + size, size == 3); // this will fail becaue we
 													// have only 3 cars in
 													// CarRepository
 	}
